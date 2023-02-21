c78225ce1d2508da886172b43e5f990c18d98c82
Ogata <ogatayuki07@gmail.com>
2023-02-21 15:12:13 +0900

    ロード機能の改修
    #26  管理番号の対応と保存していたスクリプトをロードさせる

--- BT&SM_Tool/Assets/Editor/GraphView/Load/GraphViewLoad.cs
+++ BT&SM_Tool/Assets/Editor/GraphView/Load/GraphViewLoad.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UIElements;
 /// <summary>
 /// GraphAssetの内容をエディタウィンドウに表示する
@@ -23,20 +24,26 @@ public static class GraphViewLoad
         }
     }
     private static void CreateNode(NodeData nodeData,GraphViewManager graphViewManager) {
-        var node = new TestNode();
+        var node = new ScriptNode();
         //ノードの位置
         node.SetPosition(new Rect(nodeData.position, new UnityEngine.Vector2(100,100)));
         //名前（予定）
-        //管理番号（予定）
+        //スクリプト
+        if (nodeData.Object != null)
+            node.ObjectField.value = nodeData.Object;
+        //管理番号
+        node.NodeID = nodeData.controlNumber;
+        //画面に追加
         graphViewManager.AddElement(node);
     } 
     private static void CreateEdge(EdgeData edgeData,GraphViewManager graphViewManager) {
         var node = graphViewManager.nodes.ToList();
         Debug.Log("現在生成されているノードは"+node.Count+"個です");
-        //TODO 今回は各ノードで管理番号を持っていないのでおかしくなります
-        //そもそもPortを取った方が早いかも？
-        Port inputPort = node[0].inputContainer.contentContainer.Q<Port>();
-        Port outputPort = node[1].outputContainer.contentContainer.Q<Port>();
+        //TODO　そもそもPortを取った方が早いかも？
+        //Port作製
+        Port inputPort = node[edgeData.inputNodeId].inputContainer.contentContainer.Q<Port>();
+        Port outputPort = node[edgeData.outputNodeId].outputContainer.contentContainer.Q<Port>();
+        //Edge作製
         var edge = ConnectPorts(inputPort,outputPort);
         //GraphViewに追加
         graphViewManager.AddElement(edge);
